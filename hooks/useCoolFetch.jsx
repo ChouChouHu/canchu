4e53cb1dba1e56686b8b0fb6c61a3cda23c05d63
ChouChouHu <b02705003@ntu.edu.tw>
2023-07-19 14:18:20 +0800

    feat(api): useSWR

---
+++ hooks/useCoolFetch.jsx
@@ -0,0 +1,63 @@
+import { parseCookies } from "nookies";
+import { useState } from "react";
+import useSWR, { useSWRConfig } from "swr";
+
+export default (url, options) => {
+  const cookies = parseCookies();
+  const { accessToken } = cookies;
+  const { mutate } = useSWRConfig();
+  const [isSent, setIsSent] = useState(false);
+  const { data, error } = useSWR(
+    url,
+    async (fetchUrl) => {
+      const res = await fetch(fetchUrl, {
+        headers: { authorization: `Bearer ${accessToken}` }
+      });
+      // If the status code is not in the range 200-299,
+      // we still try to parse and throw it.
+      if (!res.ok) {
+        const error = new Error("An error occurred while fetching the data.");
+        // Attach extra info to the error object.
+        error.info = await res.json();
+        error.status = res.status;
+        throw error;
+      }
+
+      return res.json();
+    },
+    {
+      onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+        console.log(`on error retry: ${key}`);
+        // Never retry on 404.
+        if (error.status === 404) return;
+
+        // Only retry up to 5 times.
+        if (retryCount >= 5) return;
+
+        // Retry after 5 seconds.
+        setTimeout(() => revalidate({ retryCount }), 5000);
+      },
+      onLoadingSlow: (key) => {
+        console.log(`on loading slow retry: ${key}`);
+        // time out to trigger refetch once
+        // note: 3s default, 5s when internet bad
+        if (!isSent) mutate(fetchUrl);
+        setIsSent(true);
+      },
+      ...options
+    }
+  );
+  if (error) {
+    console.log(error);
+  }
+
+  return { data: data?.data, error };
+};
+
+// (url) => axios({
+//     method: 'get',
+//     crossDomain: true,
+//     async: true,
+//     url
+// }),
+// 'data': res?.data.result
--- hooks/user/useUserProfile.jsx
+++ hooks/user/useUserProfile.jsx
@@ -1,34 +1,8 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { parseCookies } from "nookies";
+import useCoolFetch from "../useCoolFetch";
 
 const useUserProfile = (userId) => {
-  const [user, setUser] = useState(null);
-  // const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const cookies = parseCookies();
-      const { accessToken } = cookies;
-      try {
-        const response = await axios.get(
-          `${process.env.NEXT_PUBLIC_API_URL}/users/${userId}/profile`,
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`
-            }
-          }
-        );
-        setUser(response.data.data.user);
-      } catch (err) {
-        console.log(err.response.data || "取得用戶資料失敗")
-      }
-    };
-
-    if(userId) fetchData();
-  }, [userId]);
-
-  return { user };
+  const {data} = useCoolFetch(`${process.env.NEXT_PUBLIC_API_URL}/users/${userId}/profile`);
+  return { user: data?.user };
 };
 
 export default useUserProfile;
